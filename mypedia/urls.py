cb3f95c1a7394487ae4bb1da637af44242791948
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-12-24 13:36:34 +0300

    add CourseViewSet, LessonListCreateAPIView, LessonRetrieveUpdateDeleteView

---
+++ mypedia/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .apps import MypediaConfig
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = MypediaConfig.name
+
+router = DefaultRouter()
+router.register("courses", views.CourseViewSet, basename="courses")
+
+urlpatterns = [
+    path("lessons/", views.LessonListCreateAPIView.as_view(), name="lesson_list"),
+    path("lessons/<int:pk>/", views.LessonRetrieveUpdateDestroyAPIView.as_view(), name="lesson_detail"),
+] + router.urls
--- mypedia/views.py
+++ mypedia/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from .serializers import CourseSerializer, LessonSerializer
+from .models import Course, Lesson
+
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для CRUD операций с моделью Course
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    """
+    Дженерик для отображения списка и создания нового объекта Lesson:
+    """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Дженерик для просмотра, редактирования и удаления объекта Lesson:
+    """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
